327a26582348f77af3c87b356650ce33ed216ce5
Yusheng <yushengyang997@gmail.com>
2025-03-03 18:48:12 -0600

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # CS61B
-My solutions to UCB CS61B Fall 2024. Course website: https://fa24.datastructur.es
+My solutions to UCB CS61B Fall 2024. 
 
-## How to setup (Tips on following lab 1)
-- do not follow everything about git and github. Just clone the skeleton to your local machine.
-- DO follow the IntelliJ part. If there is warning message such as "Package xxxx does not exist.", go to ???
+## Materials
+Course website: https://fa24.datastructur.es
+Textbook: https://cs61b-2.gitbook.io/cs61b-textbook
+
+## How to setup (Tips on lab 1)
+- Do NOT follow everything about git and github. Just clone the skeleton to your local machine.
+- DO follow the IntelliJ part. However, if there is warning message like "Package xxxx does not exist.", go to 'File -> Project Structure' and select 'Libraries' under 'Project Settings' tab. Click　'+' and choose 'Java' to add the 'library-fa24' folder into your project structure. I do this to every lab and project.
---
+++ proj1a/src/LinkedListDeque61B.java
@@ -0,0 +1,62 @@
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+
+    private class Node<T> {
+        private T value;
+        private Node next;
+        private Node prev;
+    }
+
+
+    public Node head = new Node( new T, null,null);
+
+    public LinkedListDeque61B() {
+    }
+
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
