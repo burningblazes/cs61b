55fbbe3874cc801ffe49f9d9938935ac77413bfe
Yusheng <yushengyang997@gmail.com>
2025-03-14 01:13:28 -0500

    project 3a initialize

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -1,10 +1,15 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+
 public class Main {
     public static void main(String[] args) {
-
-        // build your own world!
-
+        TERenderer ter = new TERenderer();
+        ter.initialize(50,50);
+        World temp=new World(23456789098765L);
+        ter.renderFrame(temp.getWorld());
 
     }
 }
--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,57 @@
 package core;
 
+import edu.princeton.cs.algs4.StdDraw;
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+import java.util.Random;
+
 public class World {
 
-    // build your own world!
+    private static final int DEFAULT_WIDTH = 50;
+    private static final int DEFAULT_HEIGHT = 50;
+    private int width;
+    private int height;
+    private TETile[][] myWorld;
+    private Random random;
+    //private TERenderer ter;
+
+    public World(long seed) {
+        width = DEFAULT_WIDTH;
+        height = DEFAULT_HEIGHT;
+//        ter = new TERenderer();
+//        ter.initialize(width, height);
+        random = new Random(seed);
+        myWorld = new TETile[width][height];
+
+        clearWorld();
+        drawWorld();
+
+    }
+
+    public void clearWorld() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                myWorld[x][y]=Tileset.NOTHING;
+            }
+        }
+    }
+
+    public void drawWorld() {
+        //TODO: drawRooms
+        //TODO: drawWalls
+        //TODO: drawHallways
+
+        for (int x = 20; x < 35; x++) {
+            for (int y = 5; y < 10; y++) {
+                myWorld[x][y] = Tileset.WALL;
+            }
+        }
+    }
+
+    public TETile[][] getWorld() {
+        return myWorld;
+    }
+
 
 }
